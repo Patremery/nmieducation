f8e14fda98353609f691e9fd4d62d10136631256
Patrick Emery T <44465905+Patremery@users.noreply.github.com>
2025-03-05 08:37:38 +0100

    update

--- app/Helpers.php
+++ app/Helpers.php
@@ -5,6 +5,7 @@ use App\Models\GeneralSettings;
 if (! function_exists('settings')) {
     function settings($property)
     {
+        if(app()->environment('production'))
         return GeneralSettings::first()->$property;
     }
 }
