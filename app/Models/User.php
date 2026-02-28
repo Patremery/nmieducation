ee7fb93b862e02c674f25001579e7967cd82b094
Patrick Emery T <44465905+Patremery@users.noreply.github.com>
2026-02-28 16:30:11 +0100

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable implements FilamentUser
     use HasFactory, Notifiable, HasRoles;
 
     /**
-     * The attributes that are mass assignable.
+     * The attributes that are mass assignable here.
      *
      * @var array<int, string>
      */
